e14c0786388b9f2c8a6e1b16a25cebf70b01860f
plastiv <plastiv@19a8c1d6-63c1-306c-17a9-9f1537426b7b>
2011-11-27 21:12:48 +0000

    delete trunk for structure purpose

--- trunk/AsfViewer/AsfReader/AsfError.h
+++
@@ -1,9 +0,0 @@
-#pragma once
-#include <string>
-
-namespace Asf {
-
-// no classes, just functions for hadling error messages
-void error(const char* why, const char* what="");
-void error(const std::string& why, const std::string& what="");
-}
