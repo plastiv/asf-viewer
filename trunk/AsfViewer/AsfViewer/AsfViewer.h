01d4f9399d85ec74c5687e10f4ca045f164a4904
plastiv <plastiv@19a8c1d6-63c1-306c-17a9-9f1537426b7b>
2011-11-27 22:07:50 +0000

    copy all files

--- trunk/AsfViewer/AsfViewer/AsfViewer.h
+++ trunk/AsfViewer/AsfViewer/AsfViewer.h
@@ -2,7 +2,26 @@
 #define ASFVIEWER_H
 
 #include <QtGui/QMainWindow>
-#include "ui_asfviewer.h"
+#include <QtGui/QPushButton>
+#include <QtGui/QVBoxLayout>
+#include <QtGui/QHBoxLayout>
+#include <QtGui/QLabel>
+#include <QtGui/QPixmap>
+#include <QtGui/QImage>
+#include <QtGui/QWidget>
+#include <QtGui/QFileDialog>
+#include <QtGui/QMessageBox>
+#include <QtGui/QScrollArea>
+#include <QtGui/QScrollBar>
+#include <QtGui/QLineEdit>
+#include <QtGui/QStatusBar>
+#include <QtGui/QMenu>
+#include <QtGui/QAction>
+#include <QtGui/QMenuBar>
+#include "AsfLibraryWrapper.h"
+
+#include <QMainWindow>
+#include <QTimer>
 
 class AsfViewer : public QMainWindow
 {
@@ -12,8 +31,67 @@ public:
 	AsfViewer(QWidget *parent = 0, Qt::WFlags flags = 0);
 	~AsfViewer();
 
+private slots:
+	void open();
+	void closeFile();
+	void saveFile();
+	void zoomIn();
+	void zoomOut();
+	void normalSize();
+	void fitToWindow();
+	void about();
+	void numberEditChanged();
+	void goToFirstFrame();
+	void goToPrevFrame();
+	void goToNextFrame();
+	void goToLastFrame();
+	void autoPlayFrames();
 private:
-	Ui::AsfViewerClass ui;
+	void createActions();
+	void createMenus();
+	void createCentralWidget();
+	void createStatusBar();
+	void updateActions();
+	void updateButtonsRow();
+	void updateAllUi();
+	void updateCurrentFrame();
+	void scaleImage(double factor);
+	void adjustScrollBar(QScrollBar *scrollBar, double factor);
+	void goToFrame(const int frameNumber);
+
+	double scaleFactor;
+	QVector<QImage*>::const_iterator currentFrame;
+	int currentFrameNumber;
+	AsfFile* asfFile; // keep data in pool, all usage throw iterator
+	bool isFileOpen;
+
+	QLabel *imageLabel;
+	QScrollArea *scrollArea;
+	QPushButton *nextFrameBtn;
+	QPushButton *prevFrameBtn;
+	QPushButton *firstFrameBtn;
+	QPushButton *lastFrameBtn;
+	QLineEdit *frameNumEdt;
+
+	QTimer *timer;
+	QLabel *lblFrameCount;
+	QLabel *lblRows;
+	QLabel *lblCols;
+
+	QAction *openAct;
+	QAction *saveAct;
+	QAction *closeAct;
+	QAction *exitAct;
+	QAction *zoomInAct;
+	QAction *zoomOutAct;
+	QAction *normalSizeAct;
+	QAction *fitToWindowAct;
+	QAction *autoPlayAct;
+	QAction *aboutAct;
+
+	QMenu *fileMenu;
+	QMenu *viewMenu;
+	QMenu *helpMenu;
 };
 
 #endif // ASFVIEWER_H
