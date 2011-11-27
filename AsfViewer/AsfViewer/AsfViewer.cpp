0949be29bd697c1c393c8e0b202cc6d68c6d677e
plastiv <plastiv@19a8c1d6-63c1-306c-17a9-9f1537426b7b>
2011-11-27 21:18:25 +0000

    upload init structure

---
+++ AsfViewer/AsfViewer/AsfViewer.cpp
@@ -0,0 +1,13 @@
+#include "stdafx.h"
+#include "AsfViewer.h"
+
+AsfViewer::AsfViewer(QWidget *parent, Qt::WFlags flags)
+	: QMainWindow(parent, flags)
+{
+	ui.setupUi(this);
+}
+
+AsfViewer::~AsfViewer()
+{
+
+}
